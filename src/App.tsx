f632007438d6980e8e37ddfda19c36c0c027b5a1
i-am-Sirius-Black <khanjaved.sk19@gmail.com>
2024-05-03 14:36:15 +0530

    added Padding right

--- src/App.tsx
+++ src/App.tsx
@@ -44,7 +44,7 @@ function App() {
         </GridItem>
       </Show>
       <GridItem area="main">
-        <Box paddingLeft={10}>
+        <Box paddingLeft={10} paddingRight={10}>
           <GameHeading gameQuery={gameQuery}/>
           <Flex>
             <Box marginRight={5}>
